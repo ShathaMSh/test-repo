43bead3bc1f27a4557d84bb5c64b81efd22a8d8c
ShathaMSh <94394220+ShathaMSh@users.noreply.github.com>
2021-11-15 20:08:46 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'sssssss',
+        title: 'sstttt',
         theme: appTheme,
         initialRoute: '/',
         routes: {
